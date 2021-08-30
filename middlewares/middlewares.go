e5e831a86b872b79d69d21b97497995e332410d6
grandbusta <bustajay30@gmail.com>
2021-08-30 15:55:46 +0100

    middlewares updated

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -4,11 +4,28 @@ import (
 	"fmt"
 	"net/http"
 	"notekeeper/utils"
+	"os"
+	"strings"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/gorilla/mux"
 )
 
 var excluded = make([]string, 0)
+
+func verifyToken(r *http.Request) string {
+	var x string
+	bearerToken := r.Header.Get("Authorization")
+	if len(strings.Split(bearerToken, " ")) == 2 {
+		x = strings.Split(bearerToken, " ")[1]
+	}
+	token, err := jwt.Parse(x, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+		return []byte(os.Getenv("ACCESS_SECRET")), nil
+	})
+}
 
 func AuthUser(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
